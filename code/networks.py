b37f77bea1ca81a678ca16e4c61dcc583a033553
kim-jin-seop <33629459+kim-jin-seop@users.noreply.github.com>
2022-11-15 20:44:45 +0900

    modify directory

--- code/networks.py
+++ code/networks.py
@@ -1,26 +1,27 @@
 import torch.nn as nn
+import random
 
 
 class DQN(nn.Module):
     def __init__(self, actions: int):
         super(DQN, self).__init__()
         # 4 84 * 84
-        conv_1 = nn.Sequential(
+        self.conv_1 = nn.Sequential(
             nn.Conv2d(in_channels=4, out_channels=16, kernel_size=8, stride=4),
             nn.ReLU()
         )
         # 16 20 * 20
-        conv_2 = nn.Sequential(
+        self.conv_2 = nn.Sequential(
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2),
             nn.ReLU()
         )
         # 32 * 9 * 9 -> 256
-        fc_1 = nn.Sequential(
+        self.fc_1 = nn.Sequential(
             nn.Linear(32*9*9, 256),
             nn.ReLU()
         )
 
-        out_layer = nn.Sequential(
+        self.out_layer = nn.Sequential(
             nn.Linear(256, actions)
         )
 
@@ -31,3 +32,11 @@ class DQN(nn.Module):
         x = self.fc_1(x)
         x = self.out_layer(x)
         return x
+
+    def sample_action(self, obs, epsilon):
+        out = self.forward(obs)
+        coin = random.random()
+        if coin < epsilon:
+            return random.randint(0, 1)
+        else:
+            return out.argmax().item()
