60c4c900987b81299a40ed4e67da7eb4cc0a3077
kim-jin-seop <33629459+kim-jin-seop@users.noreply.github.com>
2022-11-18 22:10:07 +0900

    memory update

--- code/memory.py
+++ code/memory.py
@@ -1,23 +1,22 @@
+
 from collections import deque
-import random
-import torch
+import numpy as np
 
 
 class ReplayBuffer:
-    def __init__(self, max_buffer):
-        self.buffer = deque(maxlen=max_buffer)
+    def __init__(self, buffer_size):
+        self.buffer = deque(maxlen=buffer_size)
 
     def put(self, s, a, r, n_s, done):
         self.buffer.append((s, a, r, n_s, done))
 
     def sample(self, n):
-        batch_index = random.sample(self.buffer, n)
+        batch_index = np.random.choice(len(self.buffer), n, False)
         batch = zip(*[self.buffer[i] for i in batch_index])
         s_list, a_list, r_list, n_s_list, done_list = batch
 
-        return (torch.tensor(s_list, dtype=torch.float), torch.tensor(a_list),
-                torch.tensor(r_list), torch.tensor(n_s_list, dtype=torch.float),
-                torch.tensor(done_list))
+        return (np.array(s_list), np.array(a_list), np.array(r_list, dtype=np.float32),
+                np.array(n_s_list), np.array(done_list, dtype=np.uint8))
 
     def __len__(self):
         return len(self.buffer)
